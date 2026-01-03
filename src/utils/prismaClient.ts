1fa7d28b423fc3bc04961ab0575c61393fcecd69
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2026-01-03 18:18:32 +0400

    feat: add graceful shutdown hook.

--- src/utils/prismaClient.ts
+++ src/utils/prismaClient.ts
@@ -2,4 +2,8 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+process.on('beforeExit', async () => {
+	await prisma.$disconnect();
+});
+
 export default prisma;
