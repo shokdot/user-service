72429a7ca8c2cd5386b79afafae4445c12dce8a9
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2026-01-08 18:08:09 +0400

    feat: update to Prisma 7 ORM

--- src/utils/prismaClient.ts
+++ src/utils/prismaClient.ts
@@ -1,9 +1,14 @@
+import "dotenv/config";
 import { PrismaClient } from '@prisma/client';
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const prisma = new PrismaClient({ adapter });
 
 process.on('beforeExit', async () => {
-	await prisma.$disconnect();
+    await prisma.$disconnect();
 });
 
 export default prisma;
